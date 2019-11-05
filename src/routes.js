2610cc58d851135496b1ef9203be2576ce4df607
rogerio632 <rogeriofilho632@gmail.com>
2019-11-05 09:29:06 -0300

    add: new imports and routes

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,15 @@
 import { Router } from 'express';
 import SessionController from './app/controllers/SessionController';
+import StudentController from './app/controllers/StudentController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.post('/student/create', StudentController.store);
+routes.put('/student/update', StudentController.update);
 
 export default routes;
