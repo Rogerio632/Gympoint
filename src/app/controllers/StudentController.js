4efb15961261af91fc0e6ae0f584c75b046c2252
rogerio632 <rogeriofilho632@gmail.com>
2019-11-05 09:54:07 -0300

    add: update method

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -29,6 +29,37 @@ class StudentController {
     return res.status(201).json(req.body);
   }
 
-  async update(req, res) {}
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      id: Yup.number().required(),
+      name: Yup.string(),
+      email: Yup.string().email(),
+      age: Yup.number(),
+      weight: Yup.number(),
+      height: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Erro na validação' });
+    }
+
+    const { id, email } = req.body;
+
+    const student = await Student.findByPk(id);
+
+    if (!student) {
+      return res.status(400).json({ error: 'This student does not exists' });
+    }
+    if (email !== student.email) {
+      const studentExist = await Student.findOne({ where: { email } });
+
+      if (studentExist) {
+        return res.status(400).json({ error: 'Email already in use' });
+      }
+    }
+    const update = await student.update(req.body);
+
+    return res.json(update);
+  }
 }
 export default new StudentController();
